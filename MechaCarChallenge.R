b0ee28f9917a8fe78e95ac0047f523cffb6f7b02
Mark (Andy) Buck Jr <mabuckjr@gmail.com>
2021-11-02 20:36:41 -0700

    adding deliverable 3

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -16,3 +16,13 @@ total_summary <- Suspension_Coil %>% summarize(Mean_PSI=mean(PSI),Median_PSI=med
 #Create summary table grouped by Manufacturing Lot
 lot_summary <- Suspension_Coil %>% group_by(Manufacturing_Lot) %>% summarize(Mean_PSI=mean(PSI),Median_PSI=median(PSI),Var_PSI=var(PSI), Std_PSI=sd(PSI), .groups = 'keep')  
 
+# Deliverable 3
+## Use the t.test function to determine if the PSI across all lots is statistically different from the population mean of 1,500
+t.test(Suspension_Coil$PSI,mu=1500)
+## Write scripts to determine if the PSI for each lot is statistically different from the mean
+lot1 <- subset(Suspension_Coil, Manufacturing_Lot=="Lot1")
+lot2 <- subset(Suspension_Coil, Manufacturing_Lot=="Lot2")
+lot3 <- subset(Suspension_Coil, Manufacturing_Lot=="Lot3")
+t.test(lot1$PSI,mu=1500)
+t.test(lot2$PSI,mu=1500)
+t.test(lot3$PSI,mu=1500)
